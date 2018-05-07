7f69f57d460c9c9184384272c736eb81d87e5569
Jinpyo Kim <kchjp@naver.com>
2018-05-07 22:20:35 +0900

    Fix model definitions. Naming is according to [#6]

--- api/models.py
+++ api/models.py
@@ -34,21 +34,24 @@ def create_user(**kwargs):
 
 
 class QuestionPost(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=100)
     content = models.TextField()
-    author = models.ForeignKey('Profile', related_name='questions_as_writer', on_delete=models.CASCADE)
+    author = models.ForeignKey('Profile', related_name='questions_as_writer',
+                               on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     due = models.DateTimeField()
     resolved = models.BooleanField()
     bounty = models.IntegerField()
-    question_type = models.CharField()
-    selected = models.ForeignKey('QuestionAnswer', related_name='selected_question', on_delete=models.CASCADE)
+    question_type = models.CharField(max_length=100)
+    selected = models.ForeignKey('QuestionAnswer', related_name='selected_question',
+                                 on_delete=models.SET_NULL, null=True)
 
 
 class InformationPost(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=100)
     content = models.TextField()
-    author = models.ForeignKey('Profile', related_name='informations_as_writer', on_delete=models.CASCADE)
+    author = models.ForeignKey('Profile', related_name='informations_as_writer',
+                               on_delete=models.CASCADE)
     hidden_exist = models.BooleanField()
     hidden_content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
@@ -56,27 +59,38 @@ class InformationPost(models.Model):
     hidden_content_cost = models.IntegerField()
     sponsor_credit = models.IntegerField()
 
+
 class QuestionAnswer(models.Model):
     content = models.TextField()
-    author = models.ForeignKey('Profile', related_name='answers_as_writer', on_delete=models.CASCADE)
+    author = models.ForeignKey('Profile', related_name='answers_as_writer',
+                               on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
-    question = models.ForeignKey('QuestionPost', related_name='answers', on_delete=models.CSACADE)
+    question = models.ForeignKey('QuestionPost', related_name='answers',
+                                 on_delete=models.CASCADE)
+
 
 class Tag(models.Model):
-    name = models.CharField()
-    tag_type = models.CharField()
-    question = models.ForeignKey('QuestionPost', related_name='tags')
-    information = models.ForeignKey('InformationPost', related_name='tags')
+    name = models.CharField(max_length=100)
+    tag_type = models.CharField(max_length=100)
+    question = models.ForeignKey('QuestionPost', related_name='tags',
+                                 on_delete=models.SET_NULL, null=True)
+    information = models.ForeignKey('InformationPost', related_name='tags',
+                                    on_delete=models.SET_NULL, null=True)
+
 
 class BoughtInformation(models.Model):
-    user = models.ForeignKey('Profile', related_name='bought_informations', on_delete=models.CASCADE)
-    post = models.ForeignKey('InformationPost', related_name='buyers')
-    cost = models.NumberField()
+    user = models.ForeignKey('Profile', related_name='bought_informations',
+                             on_delete=models.CASCADE)
+    post = models.ForeignKey('InformationPost', related_name='buyers',
+                             on_delete=models.CASCADE)
+    cost = models.IntegerField()
+
 
 class Vote(models.Model):
-    vote_type = models.CharField()
-    user = models.ForeignKey('Profile', related_name='votes', on_delete=models.CASCADE)
-    post = models.ForeignKey('InformationPost', related_name='votes')
+    vote_type = models.CharField(max_length=100)
+    user = models.ForeignKey('Profile', related_name='votes',
+                             on_delete=models.CASCADE)
+    post = models.ForeignKey('InformationPost', related_name='votes',
+                             on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
-    weight = models.DecimalField()
-
+    weight = models.IntegerField()
