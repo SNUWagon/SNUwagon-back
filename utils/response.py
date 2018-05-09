63db724401df07ca7f3f85745f432e2f7f3733e2
ryanking13 <def6488@gmail.com>
2018-05-09 22:52:16 +0900

    Add response wrapper function

---
+++ utils/response.py
@@ -0,0 +1,22 @@
+from rest_framework.response import Response
+from rest_framework import status as drf_status
+
+
+def generate_response(data=None, message='', status=drf_status.HTTP_200_OK):
+
+    if status in (drf_status.HTTP_200_OK, drf_status.HTTP_201_CREATED, drf_status.HTTP_204_NO_CONTENT):
+        success = True
+    else:
+        success = False
+
+    response = {
+        'success': success,
+    }
+
+    if len(message) > 0:
+        response.update({'message': message})
+
+    if data:
+        response.update({'data': data})
+
+    return Response(response, status=status)
