2c3b12f03dff9c8f54f63a7fe8ebe7ffceee90df
Jinpyo Kim <kchjp@naver.com>
2018-05-09 15:28:35 +0900

    Implement basic Serializers. Implement question views as mentioned in [#9]. /api/question (POST), /api/question/:id (GET) are tested.

--- api/tests.py
+++ api/tests.py
@@ -2,7 +2,7 @@ import json
 from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth import authenticate
-from .models import Profile, create_user
+from .models import Profile, create_user, QuestionPost
 
 
 # put model tests here
@@ -117,3 +117,56 @@ class ApiSignUpTests(TestCase):
                                content_type='application/json')
 
         self.assertEqual(response.status_code, 401)
+
+
+class QuestionPostTests(TestCase):
+
+    def setUp(self):
+        create_user(username='testuser',
+                    password='userpassword',
+                    email='test@test.com')
+
+        # Let's create a sample question
+        client = Client()
+
+        data = {
+            'title': 'testtitle11',
+            'content': 'testcontent11',
+            'user_name': 'testuser',
+            'due': '2015-03-03T04:02:32.142923Z',
+            'resolved': False,
+            'bounty': 100,
+            'question_type': 'private'
+        }
+
+        path = reverse('question_posts')
+        client.post(path=path,
+                    data=json.dumps(data),
+                    content_type='application/json')
+
+    def test_get_question(self):
+        client = Client()
+        path = reverse('question_posts')
+        path = path + '/3'
+        response = client.get(path=path)
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_question(self):
+        client = Client()
+
+        data = {
+            'title': 'testtitle22',
+            'content': 'testcontent22',
+            'user_name': 'testuser',
+            'due': '2015-03-03T04:02:32.142923Z',
+            'resolved': False,
+            'bounty': 100,
+            'question_type': 'private'
+        }
+
+        path = reverse('question_posts')
+        response = client.post(path=path,
+                               data=json.dumps(data),
+                               content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
