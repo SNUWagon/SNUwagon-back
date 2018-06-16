2bc4f506a01887d50ae13b491fb271808d7e6e59
Jinpyo Kim <kchjp@naver.com>
2018-06-16 21:37:41 +0900

    Fix issue #105 - (5) (6). Email verification.

--- api/views/auth_views.py
+++ api/views/auth_views.py
@@ -12,6 +12,7 @@ from django.core.mail import EmailMessage
 from hashlib import md5
 import string
 import random
+from django.shortcuts import redirect
 
 
 @swagger_auto_schema(methods=['post'], request_body=UserSerializer, responses={201: 'success'})
@@ -60,7 +61,7 @@ def signup(request):
             return generate_response(message='Duplicate username or email', status=status.HTTP_401_UNAUTHORIZED)
 
         mail_subject = '[SNUWagon] Please confirm your email'
-        message = 'http://localhost:8000/api/auth/verification/' + hashstring
+        message = 'http://www.snuwagon.com:8000/api/auth/verification/' + hashstring
         to = [email]
         email = EmailMessage(
             mail_subject, message, to=to
@@ -76,8 +77,7 @@ def verification(request, hashstring):
     profile = Profile.objects.get(hashstring=hashstring)
     profile.verified = True
     profile.save()
-
-    return generate_response(message='Authentication successful', status=status.HTTP_201_CREATED)
+    return redirect('http://www.snuwagon.com:3000/')
 
 
 @swagger_auto_schema(methods=['get'], responses={200: 'success'})
